c756faae265014063802786d209c982ee8cfc23e
benjaaguileragit-pixel <benjaaguileragit@gmail.com>
2026-04-07 09:13:20 -0300

    Add files via upload

---
+++ src/app/compartidos/piedepagina/piedepagina.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-piedepagina',
+  imports: [],
+  templateUrl: './piedepagina.html',
+  styleUrl: './piedepagina.css',
+})
+export class Piedepagina {}
