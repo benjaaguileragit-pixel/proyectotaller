206585ceaf2c93e84d9e0db3421c6a0c699fd288
benjaaguileragit-pixel <benjaaguileragit@gmail.com>
2026-04-16 09:41:56 -0300

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './paginas/inicio/inicio';
+import { Carrito } from './compartidos/carrito/carrito';
+import { Fav } from './compartidos/fav/fav';
+import { Ofertas } from './paginas/ofertas/ofertas';
+import path from 'path';
+import { Iniciosesion } from './auth/iniciosesion/iniciosesion';
+import { Registro } from './auth/registro/registro';
+import { Contacto } from './paginas/contacto/contacto';
+import { Productos } from './paginas/productos/productos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: "", redirectTo:"Inicio",pathMatch:"full"},
+    {path: "Inicio", component: Inicio},
+    {path: "Carrito",component: Carrito},
+    {path: "Fav",component: Fav},
+    {path: "Ofertas", component: Ofertas},
+    {path:"Iniciosesion", component: Iniciosesion},
+    {path: "Registro", component: Registro},
+    {path: "Contacto", component: Contacto},
+    {path: "Productos", component: Productos}
+];
---
+++ src/app/compartidos/fav/fav.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fav',
+  imports: [],
+  templateUrl: './fav.html',
+  styleUrl: './fav.css',
+})
+export class Fav {}
